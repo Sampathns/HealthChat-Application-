06ec668542a0eba6838460eb69918a9e420bcbc1
Sampathns <nssampathn@gmail.com>
2026-05-25 21:40:35 +0530

    Your message

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
+
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? 'https://healthchat-application.onrender.com' 
+    : 'http://localhost:5000'
 });
 
 // Request interceptor - add auth token
