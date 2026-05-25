124c733c7a8ae71bd2a77ea6f7d4a547b0093d59
Sampathns <nssampathn@gmail.com>
2026-05-25 21:57:42 +0530

    Your message

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://healthchat-application-tgt1.onrender.com";
 
 const api = axios.create({
-
   baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://healthchat-application.onrender.com' 
-    : 'http://localhost:5000'
+    ? 'https://healthchat-application-tgt1.onrender.com' 
+    : 'http://localhost:5000' // මෙන්න මේක අනිවාර්යයෙන්ම දාන්න!
 });
 
 // Request interceptor - add auth token
