9bdf3b9425ad71246ec5651cee057c2bd4a7c836
Sampathns <nssampathn@gmail.com>
2026-05-25 15:30:03 +0530

    Fixed api base url with api prefix and removed white space

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || " https://healthchat-application-1.onrender.com";
+
+const API_URL = process.env.REACT_APP_API_URL || "https://healthchat-application-1.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
